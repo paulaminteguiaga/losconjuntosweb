f5216c5cad96c61b252c5eaf30629fd88fb20f0d
Paula Minteguiaga <paula.minteguiaga@mercadolibre.com>
2026-04-20 22:03:39 -0300

    remove docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,4 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://paulaminteguiaga.github.io',
-  base: '/losconjuntosweb',
 });
