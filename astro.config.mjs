2adb08b3502fab83025a5972388d5411cda6df83
Paula Minteguiaga <paula.minteguiaga@mercadolibre.com>
2026-04-20 21:44:51 -0300

    add build for github pages on docs/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://paulaminteguiaga.github.io',
+  base: '/losconjuntosweb',
 });
